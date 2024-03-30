1c3c37b198408b9eca674f1aced120c75d26b462
003-Aman <amanshrestha3003@gmail.com>
2024-03-29 22:42:31 -0400

    Graph simple explanation

--- Graph.py
+++ Graph.py
@@ -24,6 +24,18 @@ Minimum Spanning Tree applications:
 to find the shortest path between two nodes in a graph
 in a map
 to design networks like telecommunications,water supply, electrical grids
+
+Adjacancey list:
+an adjacency list represents a graph as an array of linked lists
+the index of the array represents a vertex and the other vertices that form an edge with the vertex
+this is easier to understand when you look at the diagrams
+
+pros:
+is efficient in terms of storage because we onlu need to store the values of the edges
+it also helps to find all the vertices adjacet to a vertex easily
+
+cons:
+finding in the adjacent  list is not quicket than the adjacenty matrix because all the connected nodes must be first explored to find them
 """
 class AdjacencyMatrix:
     def __init__(self, vertices):
@@ -44,6 +56,29 @@ class AdjacencyMatrix:
             for j in range(self.vertices):
                 print(self.matrix[i][j], end=" ")
             print()
+
+
+
+class AdjacencyList:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.adjacencyList = [[] for _ in range(vertices)]
+
+    def addEdge(self, u, v):
+        self.adjacencyList[u].append(v)
+
+    def checkEdge(self, u, v):
+        return v in self.adjacencyList[u]
+
+    def removeEdge(self, u, v):
+        self.adjacencyList[u].remove(v)
+
+    def printList(self):
+        for i in range(self.vertices):
+            print(i, "-->", self.adjacencyList[i])
+
+
+
 
 
 
