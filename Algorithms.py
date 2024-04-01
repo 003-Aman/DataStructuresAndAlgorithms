10c23e72a9c854fd7e0f46c2f478efe8ae0d68fd
003-Aman <amanshrestha3003@gmail.com>
2024-03-31 22:28:09 -0400

    algorithms

---
+++ Algorithms.py
@@ -0,0 +1,27 @@
+#DIJKSTRA'S ALGORITHM
+'''
+dijkstra's algorithm is an algorithm for finding the shortest paths between nodes in a graph
+it is a greedy algorithm
+it works on breadth first search
+where it starts from the root node and explores all the nodes at the present depth prior to moving on to the nodes at the next depth level
+then it gives the minimum sum of weights of the edges
+
+'''
+
+# BELLMAN FORD ALGORITHM
+'''
+bellman ford algorithm is an algorithm for finding the shortest paths between nodes in a graph
+it is a dynamic programming algorithm 
+it works where dijkstra doesnt
+it helps to find the minimum weight when the weight of the edges are negative
+'''
+
+#MINIMUM SPANNING TREE
+'''
+minimum spanning tree is a subgraph that is a tree and has as many nodes as possible
+
+'''
+#PRIM'S ALGORITHM
+'''
+prims algo find the minimum spanning tree from a graph
+'''
