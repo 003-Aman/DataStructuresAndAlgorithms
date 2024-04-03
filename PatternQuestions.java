db7b7026c1958583f62a6aa6041cb58c12f06329
003-Aman <amanshrestha3003@gmail.com>
2024-04-03 14:58:34 -0400

    Pattern Questions

--- PatternQuestions.java
+++ PatternQuestions.java
@@ -65,6 +65,18 @@ public class PatternQuestions {
             }
             System.out.println();
         }
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=i;j++){
+                int sum =i+j;
+                if(sum%2==0){
+                    System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
 
 
 
