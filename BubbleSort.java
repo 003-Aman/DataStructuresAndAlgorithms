a8d8a089e2f96a0706ceb3be95ec631b2c8c3fa9
003-Aman <amanshrestha3003@gmail.com>
2024-04-05 21:10:50 -0400

    Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class BubbleSort {
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]={7,8,3,1,2};
+        //bubble sort
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if (arr[j]>arr[j+1]){
+                    int temp = arr[j];//swapping the variables, storing left element in temp variable
+                    arr[j]=arr[j+1]; //changing left to right
+                    arr[j+1]= temp;//right varible to temp
+                }
+            }
+
+        }
+
+        printArray(arr);
+
+    }
+    
+    
+}
