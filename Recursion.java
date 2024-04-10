f75078440850cbb20b76cb205b4b352afc4e14b9
003-Aman <amanshrestha3003@gmail.com>
2024-04-09 20:37:30 -0400

    Recursion

--- Recursion.java
+++ Recursion.java
@@ -43,7 +43,29 @@ public class Recursion{
         System.out.println(a);
         printFactorial(b, a+b, n-1);
         }
-    
+
+    public static int calcPower(int x,int n){
+        if(n==0){
+            return 1;
+        }
+        if(x==0){
+            return 0;
+        }
+        int xPown = x*calcPower(x, n-1);
+        return xPown;
+    }
+    public static int printPower(int x, int n) {
+        if(n == 0) {
+        return 1;
+        }
+        if(n % 2 == 0) {
+        return printPower(x, n/2) * printPower(x, n/2);
+        }
+        else {
+        return x * printPower(x, n/2) * printPower(x, n/2);
+        }
+        }
+
     public static void main(String[] args){
         int n=7;
         int number =1;
@@ -55,5 +77,8 @@ public class Recursion{
         System.out.println(a);
         System.out.println(b);
         printFactorial(0, 1, 5);
+        int x = 2, num = 5;
+        int output = printPower(x, num);
+        System.out.println(output);
     }
 }
